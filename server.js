7bf5926c3754dd5a95f321aa226edb42a139b29b
Baptiste Etienne <baptiste.etienne@etu.unice.fr>
2016-01-31 19:45:38 +0100

    aze

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get('/', function(req, res){
     if(req.session.authid){
         res.redirect('/main');
     }else{
-        res.render('/login');
+        res.render('login', {errors: []});
     }
 });
 
@@ -77,6 +77,55 @@ app.post('/login', function(req, res) {
         }
     }
 });
+
+
+
+
+app.get('/register', function(req, res){
+    res.render('register', {errors: []});
+});
+
+app.post('/login', function(req, res) {
+    if(req.session.userid){
+        res.render('main');
+    }else {
+        var bodyEmail = req.body.email;
+        var bodyPassword = req.body.password;
+        var errors = [];
+
+        if (!validator.isEmail(bodyEmail)) {
+            errors.push('Email invalide ');
+        }
+        if (!validator.isAlphanumeric(bodyPassword)) {
+            errors.push('Password invalide ');
+        }
+
+        if (errors.length != 0) {
+            res.render('login', {errors: errors});
+        }
+        else {
+
+
+            var data = {email: bodyEmail, password: bodyPassword};
+
+            //db
+            DB.login(req, res, data, function (rows) {
+                if (rows.length == 0) {
+                    errors.push('Email ou mot de passe incorrect !');
+                    res.render('login', {errors: errors});
+                }
+                else {
+                    req.session.userid=rows[0].id;
+                    res.redirect('/main');
+                }
+            });
+        }
+    }
+});
+
+
+
+
 
 app.get('/main', function(req, res){
     DB.profil(req,res,function(rows)
